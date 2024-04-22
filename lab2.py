f6d6dfb13caaf4b5ed03b3a1d61e08c1cf5f2445
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-04-22 15:51:31 +0800

    n/a

--- lab2.py
+++ lab2.py
@@ -1,18 +1,23 @@
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
-    sort_temperature(get_user_input())
+    num_list = get_user_input()
+    sorted_list = sort_temperature(num_list)
+    calc_average_temperature(num_list)
+    find_min_max(sorted_list)
+
 
 def get_user_input():
     x = input()
     numbers_list = list(map(float, x.split(", ")))
     return numbers_list
 
-def calc_average_temeperature(sorted_list):
-    print("calc_average_temperature")
-    average = sum(sorted_list)
+def calc_average_temperature(sorted_list):
+    total = sum(sorted_list)
     num_elements = len(sorted_list)
+    average = total/num_elements
+    return average
     
-def find_min_max():
+def find_min_max(sorted_list):
     print("find_min_max")
 
 def sort_temperature(temperature_list):
