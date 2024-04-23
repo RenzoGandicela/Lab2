84e5c823e83eb7b66bb62c2ee3e66c84b11cd2f7
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-04-23 19:29:43 +0800

    Exercise 6 finished!

--- lab2.py
+++ lab2.py
@@ -1,14 +1,19 @@
-def display_main_menu():
-    print("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
     num_list = get_user_input()
     sorted_list = sort_temperature(num_list)
-    calc_average_temperature(num_list)
+    print("Average = " + str(calc_average_temperature(num_list)))
     find_min_max(sorted_list)
+    print("Median = " + str(calc_median_temperature(sorted_list)))
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
 
 def get_user_input():
     x = input()
-    numbers_list = list(map(float, x.split(", ")))
-    return numbers_list
+    num_list = list(map(float, x.split(", ")))
+    return num_list
 
 def calc_average_temperature(sorted_list):
     total = sum(sorted_list)
@@ -17,11 +22,20 @@ def calc_average_temperature(sorted_list):
     return average
     
 def find_min_max(sorted_list):
-    print("Min. = " + sorted_list[0])
+    print("Min. = " + str(sorted_list[0]))
+    print("Max. = " + str(sorted_list[-1]))
 
 def sort_temperature(num_list):
-    sorted_list = sorted(num_list)
+    sorted_list = sorted(num_list)  #sorted() func sorts items in a list in ascending order
     return sorted_list
 
-def calc_median_temperature():
-    print("calc_median_temperature")
+def calc_median_temperature(sorted_list):
+    n = len(sorted_list)
+    if n % 2 == 1: # Odd number of elements: return the middle element
+        median = sorted_list[n//2]
+    else: # Even number of elements: return the average of the middle two elements
+        median = (sorted_list[(n//2)-1] + sorted_list[n//2]) / 2
+    return median
+
+if __name__ == "__main__" :
+    main()
